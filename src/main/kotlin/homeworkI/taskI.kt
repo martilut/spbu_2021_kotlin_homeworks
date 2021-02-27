4ab249bae5b01189cd9a967dc019c41348965f5f
Alena <jeykolton@gmail.com>
2021-02-27 19:29:05 +0300

    task_1.1 added

---
+++ src/main/kotlin/homeworkI/taskI.kt
@@ -0,0 +1,35 @@
+package homeworkI
+
+fun iterativeFactorial(n: Int): Int {
+    var answer = 1
+    var count = 0
+    repeat(n) {
+        answer *= (++count)
+    }
+    return answer
+}
+
+fun recursiveFactorial(n: Int): Int {
+    if (n == 0) {
+        return 1
+    }
+    return n * recursiveFactorial(n - 1)
+}
+
+val scan = java.util.Scanner(System.`in`)
+
+fun getInput(): Int {
+    var n = scan.nextInt()
+    while (n < 0) {
+        print("Your number must be positive: ")
+        n = scan.nextInt()
+    }
+    return n
+}
+
+fun main() {
+    print("Enter your number: ")
+    val n = getInput()
+    println("Iterative factorial is ${iterativeFactorial(n)}")
+    println("Recursive factorial is ${recursiveFactorial(n)}")
+}
