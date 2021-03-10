88758686cc60960991967aa2e3b4f0cd93724655
Alena <jeykolton@gmail.com>
2021-03-10 21:14:26 +0300

    task added

---
+++ src/main/kotlin/homework2/task_2.kt
@@ -0,0 +1,29 @@
+package homework2
+
+fun removeDuplicateElements(elements : MutableList<Int>) : Set<Int> {
+    return elements.reversed().toSet()
+}
+
+fun inputElements() : MutableList<Int> {
+    val scan = java.util.Scanner(System.`in`)
+    val elements = mutableListOf<Int>()
+    print("Enter the amount of elements: ")
+    val size = scan.nextInt()
+    print("Enter your elements: ")
+    for (i in 1..size) {
+        elements.add(scan.nextInt())
+    }
+    return elements
+}
+
+fun outputElements(elements: List<Int>) {
+    for (element in elements) {
+        print("$element ")
+    }
+}
+
+fun main() {
+    val elements = inputElements()
+    val newList = removeDuplicateElements(elements).reversed()
+    outputElements(newList)
+}
