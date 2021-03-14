b02192d57ba26d8ef7ba5082c8aac71fef13f335
Alena <jeykolton@gmail.com>
2021-03-14 23:06:07 +0300

    serialization plugin added

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.30"
+    kotlin("plugin.serialization") version "1.4.30"
     id("io.gitlab.arturbosch.detekt") version "1.15.0"
     application
+
 }
 
 group = "me.user"
@@ -15,6 +17,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.1.0")
     testImplementation(kotlin("test-junit"))
 
     detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.14.2")
