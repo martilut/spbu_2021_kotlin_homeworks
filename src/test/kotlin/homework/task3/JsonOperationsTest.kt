8c5b62a81f051070cd88805845900eb912463505
Alena <jeykolton@gmail.com>
2021-03-31 16:37:56 +0300

    jsonOperationsTest fixed actions added

--- src/test/kotlin/homework/task3/JsonOperationsTest.kt
+++ src/test/kotlin/homework/task3/JsonOperationsTest.kt
@@ -2,26 +2,29 @@ package homework.task3
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.io.TempDir
 import java.io.File
+import java.nio.file.Path
 
 internal class JsonOperationsTest {
 
     @Test
     fun loadFromJson() {
-        val jsonFile = File("src/test/resources/inputFile.json")
+        val jsonFile = File(javaClass.getResource("loadFromJsonTest.json").path)
         val performedCommandStorage = PerformedCommandStorage()
-        jsonFile.writeText("[{\"type\":\"InsertToStart\",\"value\":1},{\"type\":\"InsertToEnd\",\"value\":2}]")
         JsonOperations(jsonFile).loadFromJson(performedCommandStorage)
         assertEquals(mutableListOf(1, 2), performedCommandStorage.elements)
     }
 
     @Test
-    fun saveToJson() {
-        val jsonFile = File("src/test/resources/inputFile.json")
+    fun saveToJson(@TempDir tempDir: Path) {
+        val expectedFile = File(javaClass.getResource("saveToJsonTest.json").path)
+        val tempFile = File(tempDir.resolve("tempFile.json").toString())
         val performedCommandStorage = PerformedCommandStorage()
         performedCommandStorage.makeAction(InsertToStart(1))
         performedCommandStorage.makeAction(InsertToEnd(2))
-        JsonOperations(jsonFile).saveToJson(performedCommandStorage)
-        assertEquals("[{\"type\":\"InsertToStart\",\"value\":1},{\"type\":\"InsertToEnd\",\"value\":2}]", jsonFile.readText())
+        JsonOperations(tempFile).saveToJson(performedCommandStorage)
+        assertEquals(expectedFile.readText(), tempFile.readText())
+        tempFile.delete()
     }
 }
