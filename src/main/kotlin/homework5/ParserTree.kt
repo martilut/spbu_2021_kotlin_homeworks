44a6936d74894bf6ddde1717553520cfbe914258
Alena <jeykolton@gmail.com>
2021-04-16 17:34:34 +0300

    parser function changed

--- src/main/kotlin/homework5/ParserTree.kt
+++ src/main/kotlin/homework5/ParserTree.kt
@@ -1,47 +1,22 @@
 package homework5
 
 interface ParserTreeNode {
-    val isEmpty: Boolean
-    val isCounted: Boolean
     fun calculate(): Int
-    fun getOperator(): String?
     fun outputValue(height: Int): String
 }
 
-class OperandNode(private val value: Int, private val isEmptyPrivate: Boolean = false) : ParserTreeNode {
-    override val isEmpty: Boolean
-        get() = isEmptyPrivate
-    override val isCounted: Boolean
-        get() = true
+class OperandNode(private val value: Int) : ParserTreeNode {
     override fun calculate(): Int = value
-    override fun getOperator(): String? = null
     override fun outputValue(height: Int): String =
         ".".repeat(height) + this.value.toString() + "\n"
 }
 
-class OperatorNode(private val operator: String?) : ParserTreeNode {
-    override val isEmpty: Boolean
-        get() = false
-    private var leftChild: ParserTreeNode = OperandNode(0, true)
-    private var rightChild: ParserTreeNode = OperandNode(0, true)
-
-    private var _isCounted = false
-    override val isCounted: Boolean
-        get() = _isCounted
-
-    fun changeIsCounted() {
-        _isCounted = true
-    }
-
-    fun changeChildren(newLeft: ParserTreeNode, newRight: ParserTreeNode) {
-        leftChild = newLeft
-        rightChild = newRight
-    }
-
+class OperatorNode(
+    private val operator: String?,
+    private var leftChild: ParserTreeNode,
+    private var rightChild: ParserTreeNode
+    ) : ParserTreeNode {
     override fun calculate(): Int {
-        if (leftChild.isEmpty || rightChild.isEmpty) {
-            throw IllegalArgumentException("Your expression is incorrect")
-        }
         return when (operator) {
             OperationType.PLUS.value -> leftChild.calculate() + rightChild.calculate()
             OperationType.MINUS.value -> leftChild.calculate() - rightChild.calculate()
@@ -59,71 +34,37 @@ class OperatorNode(private val operator: String?) : ParserTreeNode {
         }
     }
 
-    override fun getOperator(): String? = operator
-
     override fun outputValue(height: Int): String {
-        if (leftChild.isEmpty || rightChild.isEmpty) {
-            throw IllegalArgumentException("Your expression is incorrect")
-        }
         return ".".repeat(height) + this.operator + "\n" +
                 this.leftChild.outputValue(height * 2) +
                 this.rightChild.outputValue(height * 2)
     }
 }
 
-class TreeBuilder(expression: List<String>) {
-    val treeRoot: ParserTreeNode = getParseTreeRoot(expression)
-
-    private fun addNewNode(lexeme: String, list: MutableList<ParserTreeNode>) {
-        val value = lexeme.toIntOrNull()
-        when {
-            value != null -> list.add(OperandNode(value))
-            operationContains(lexeme) != null -> list.add(OperatorNode(lexeme))
-            else -> throw IllegalArgumentException("Your operators or operands are incorrect")
-        }
-    }
-
-    private fun updateNodeList(list: MutableList<ParserTreeNode>) {
-        val secondNode = list.removeLast()
-        val firstNode = list.removeLast()
-        val parentNode = list.removeLast()
-        val nodeToAdd = OperatorNode(parentNode.getOperator())
-        nodeToAdd.changeChildren(firstNode, secondNode)
-        try {
-            nodeToAdd.calculate()
-        } catch (e: IllegalArgumentException) {
-            throw IllegalArgumentException(e.message)
-        }
-        nodeToAdd.changeIsCounted()
-        list.add(nodeToAdd)
-    }
-
-    private fun updateNode(node: ParserTreeNode): Boolean = node.isCounted && !node.isEmpty
-
-    private fun isReadyToUpdate(list: MutableList<ParserTreeNode>): Boolean {
-        val size = list.size
-        return size > 2 && updateNode(list[size - 1]) && updateNode(list[size - 2])
-    }
-
-    private fun getParseTreeRoot(expression: List<String>): ParserTreeNode {
-        val nodeList = mutableListOf<ParserTreeNode>()
-        for (lexeme in expression) {
-            addNewNode(lexeme, nodeList)
-            while (isReadyToUpdate(nodeList)) {
-                updateNodeList(nodeList)
-            }
-        }
-        if (nodeList.size != 1) {
-            throw IllegalArgumentException("Your expression is incorrect")
-        }
-        return nodeList[0]
-    }
-}
-
-class ParserTree(expression: List<String>) {
-    private var root = TreeBuilder(expression).treeRoot
+class ParserTree(inputExpression: List<String>) {
+    private val root = parseExpression(inputExpression).treeRoot
 
     fun getResult(): Int = root.calculate()
-
     fun getOutputTree(): String = root.outputValue(1)
+
+    data class ParserData(val treeRoot: ParserTreeNode, val currentList: List<String>)
+
+    private fun parseExpression(expression: List<String>): ParserData {
+        return when {
+            expression.first().toIntOrNull() != null -> {
+                ParserData(OperandNode(expression.first().toInt()), expression.drop(1))
+            }
+            else -> {
+                val operator = expression.first()
+                if (operationContains(operator) == null) {
+                    throw IllegalArgumentException("Your operators or operands are incorrect")
+                }
+                var currentData = parseExpression(expression.drop(1))
+                val left = currentData.treeRoot
+                currentData = parseExpression(currentData.currentList)
+                val right = currentData.treeRoot
+                return ParserData(OperatorNode(operator, left, right), currentData.currentList)
+            }
+        }
+    }
 }
