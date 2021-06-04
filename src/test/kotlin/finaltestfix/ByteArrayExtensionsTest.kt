8bbe7294b3b014b4d349a72ef0bd8c8451dbe3e3
Alena <jeykolton@gmail.com>
2021-06-04 12:52:56 +0300

    import fixed

--- src/test/kotlin/finaltestfix/ByteArrayExtensionsTest.kt
+++ src/test/kotlin/finaltestfix/ByteArrayExtensionsTest.kt
@@ -1,6 +1,6 @@
 package finaltestfix
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertArrayEquals
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
